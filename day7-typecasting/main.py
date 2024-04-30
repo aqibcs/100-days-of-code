a6ff2cccfad81834c68330c2045de5e8bad1e577
aqibcs <aqibshahcs@gmail.com>
2024-04-30 16:20:43 +0500

    Typecasting in python

---
+++ day7-typecasting/main.py
@@ -0,0 +1,14 @@
+# Explicit typecasting from string to int
+string_num = "15"
+integer_num = int(string_num)
+print("Integer Number:", integer_num)
+print("Type of Integer Number:", type(integer_num))
+
+
+# Implicit typecasting from int to float during addition
+num_int = 7
+num_float = 3.0
+result = num_int + num_float
+print("Result:", result)
+print("Type of Result:", type(result))
+
