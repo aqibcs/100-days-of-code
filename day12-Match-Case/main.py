3b6767679f23ae97d98d3d56d875f51d05d5a3d3
aqibcs <aqibshahcs@gmail.com>
2024-05-03 12:58:20 +0500

    Match Cases

---
+++ day12-Match-Case/main.py
@@ -0,0 +1,13 @@
+# Define the variable fruit
+fruit = "apple"
+
+# Match the variable fruit using match case
+match fruit:
+    case "apple":
+        print("It's an apple!")
+    case "banana":
+        print("It's a banana!")
+    case "orange":
+        print("It's an orange!")
+    case _:
+        print("Unknown fruit!")
