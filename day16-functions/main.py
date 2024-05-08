7b085b6c7b38eb6418b1b736781329b0bbf21657
aqibcs <aqibshahcs@gmail.com>
2024-05-08 07:49:33 +0500

    Python Functions

---
+++ day16-functions/main.py
@@ -0,0 +1,16 @@
+# Define a function that greets a user
+def greet_user(name):
+    print("Hello", name)
+
+# Call the function with an argument
+greet_user("Aqib")
+
+
+# Define function to add two numbers
+def add_number(a, b):
+    sum = a + b
+    return sum
+
+# Call the function and print the result
+result = add_number(5, 10)
+print("Sum:", result)
